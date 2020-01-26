e8ffa6b2455d1d501f36dd6eb03a03a9fbc89241
Dan Corder <dan.corder@softwire.com>
2020-01-26 14:33:09 +0000

    wip

---
+++ Validators/GovUkIntegerValidator2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GovUkDesignSystem.Validators
+{
+    public class GovUkIntegerValidator2 : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return new ValidationResult("qq:DCC Failed validation 2");
+        }
+    }
+}
