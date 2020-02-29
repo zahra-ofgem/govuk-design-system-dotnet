42d87126aacd72ce520328cd1a93097a3e27a971
Dan Corder <dan.corder@ofgem.gov.uk>
2020-02-29 11:46:26 +0000

    feat: Add default snapshot test project

---
+++ GovUkDesignSystem.SnapshotTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GovUkDesignSystem.SnapshotTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
