92bf02ec09e846c3ed46c4a72c03596a3cac67da
Dan Corder <dan.corder@ofgem.gov.uk>
2020-03-01 00:24:05 +0000

    feat: Move snapshots to subfolder

--- GovUkDesignSystem.SnapshotTests/ApprovalTestsConfig.cs
+++ GovUkDesignSystem.SnapshotTests/ApprovalTestsConfig.cs
@@ -1,4 +1,6 @@
-﻿using ApprovalTests.Reporters;
+﻿using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
 using ApprovalTests.Reporters.Windows;
 
 [assembly: UseReporter(typeof(WinMergeReporter))]
+[assembly: UseApprovalSubdirectory("Snapshots")] 
